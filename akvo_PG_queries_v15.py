550b4e676144daa06ef59acc4aa0d644eeb9a641
Edmond Muller <edmond@edmonds-mbp.kpn>
2023-01-07 08:27:49 +0100

    third version

--- akvo_PG_queries_v15.py
+++ akvo_PG_queries_v15.py
@@ -928,8 +928,8 @@ DROP POLICY IF EXISTS mkec_policy ON calc_tab_tree_submissions_per_contract;
 
 CREATE POLICY mkec_policy ON akvo_tree_registration_areas_updated TO kenya_mkec USING (organisation = 'Mount Kenya Environmental Conservation');
 CREATE POLICY mkec_policy ON akvo_tree_monitoring_areas TO kenya_mkec USING (EXISTS (SELECT * FROM akvo_tree_monitoring_areas
-WHERE akvo_tree_registration_areas_updated.organisation = 'Mount Kenya Environmental Conservation')
-AND akvo_tree_monitoring_areas.identifier_akvo = akvo_tree_registration_areas_updated.identifier_akvo);
+WHERE akvo_tree_registration_areas_updated.organisation = 'Mount Kenya Environmental Conservation'
+AND akvo_tree_monitoring_areas.identifier_akvo = akvo_tree_registration_areas_updated.identifier_akvo));
 CREATE POLICY mkec_policy ON akvo_nursery_registration TO kenya_mkec USING (organisation = 'Mount Kenya Environmental Conservation');
 --CREATE POLICY mkec_policy ON akvo_nursery_monitoring TO kenya_mkec USING (organisation = 'Mount Kenya Environmental Conservation');
 CREATE POLICY mkec_policy ON CALC_TAB_Error_partner_report_on_site_registration TO kenya_mkec USING (CALC_TAB_Error_partner_report_on_site_registration.name_organisation = 'Mount Kenya Environmental Conservation');
