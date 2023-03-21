42fead50cb74a6bd7b6eaff5561345b828c259a0
Edmond Muller <edmond@edmonds-mbp.kpn>
2023-03-21 16:51:47 +0100

    updated contract overview table a15

--- akvo_PG_queries_v21.py
+++ akvo_PG_queries_v21.py
@@ -1200,7 +1200,7 @@ WHERE akvo_ecosia_nursery_registration.organisation = 'Mount Kenya Environmental
 AND akvo_ecosia_nursery_monitoring.identifier_akvo = akvo_ecosia_nursery_registration.identifier_akvo));
 CREATE POLICY mkec_policy ON error_partner_report_on_site_registration TO kenya_mkec USING (error_partner_report_on_site_registration.name_organisation = 'Mount Kenya Environmental Conservation');
 CREATE POLICY mkec_policy ON error_partner_report_on_nursery_registration TO kenya_mkec USING (error_partner_report_on_nursery_registration.organisation = 'Mount Kenya Environmental Conservation');
-CREATE POLICY mkec_policy ON akvo_ecosia_contract_overview TO kenya_mkec USING (akvo_ecosia_contract_overview.name_organisation = 'mount kenya environmental conservation');
+CREATE POLICY mkec_policy ON akvo_ecosia_contract_overview TO kenya_mkec USING (akvo_ecosia_contract_overview.organisation = 'mount kenya environmental conservation');
 CREATE POLICY mkec_policy ON akvo_ecosia_tree_photo_registration TO kenya_mkec USING (akvo_ecosia_tree_photo_registration.organisation = 'Mount Kenya Environmental Conservation');'''
 
 
@@ -1260,7 +1260,7 @@ WHERE akvo_ecosia_nursery_registration.organisation = 'Fundacion DIA'
 AND akvo_ecosia_nursery_monitoring.identifier_akvo = akvo_ecosia_nursery_registration.identifier_akvo));
 CREATE POLICY fdia_policy ON error_partner_report_on_site_registration TO nicaragua_fdia USING (error_partner_report_on_site_registration.name_organisation = 'Fundacion DIA');
 CREATE POLICY fdia_policy ON error_partner_report_on_nursery_registration TO nicaragua_fdia USING (error_partner_report_on_nursery_registration.organisation = 'Fundacion DIA');
-CREATE POLICY fdia_policy ON akvo_ecosia_contract_overview TO nicaragua_fdia USING (akvo_ecosia_contract_overview.name_organisation = 'fundacion dia');
+CREATE POLICY fdia_policy ON akvo_ecosia_contract_overview TO nicaragua_fdia USING (akvo_ecosia_contract_overview.organisation = 'fundacion dia');
 CREATE POLICY fdia_policy ON akvo_ecosia_tree_photo_registration TO nicaragua_fdia USING (akvo_ecosia_tree_photo_registration.organisation = 'Fundacion DIA');'''
 
 conn.commit()
