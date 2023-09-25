cfe7deeae3c8bcec48ea04d8f20313b698a9bc62
Edmond Muller <edmond@edmonds-mbp.kpn>
2023-09-25 17:34:35 +0200

    new upload

--- akvo_PG_queries_v27.py
+++ akvo_PG_queries_v27.py
@@ -3817,7 +3817,7 @@ cur.execute(create_a43)
 cur.execute(create_a44)
 cur.execute(create_a45)
 cur.execute(create_a46)
-cur.execute(create_a47)
+#cur.execute(create_a47)
 cur.execute(create_a48)
 cur.execute(create_a49)
 
