ba089e2719eca9ae79e073818119b798c0f38734
Edmond Muller <edmond@edmonds-mbp.kpn>
2024-11-05 18:38:23 +0100

    add odk script

--- ODK_create_entities_with_merge_airtable_heroku.py
+++ ODK_create_entities_with_merge_airtable_heroku.py
@@ -57,6 +57,7 @@ data_contracts = response.json()
 for ids in data_contracts['records']:
     try:
         contract_id = ids['fields']['ID']
+        contract_id = str(contract_id)+'.00'
         confirmation_monitoring = ids['fields']['monitor?'] # Returns true if activated. If not activated it loops into the python Except
         if confirmation_monitoring is True:
             list_contracts.append(contract_id)
@@ -72,7 +73,7 @@ list_offsets.append(offset)
 
 # Loop through the first (again), second and subsequent 100 row pages and collect the offset string codes and put them into the offset list
 for offset_loop in list_offsets:
-    url_contracts = os.environ["URL_AIRTABLE_CONTRACTS"] + "?offset=" + offset_loop
+    url_contracts = "https://api.airtable.com/v0/appkx2PPsqz3axWDy/Contracts" + "?offset=" + offset_loop
     response = requests.get(url_contracts, headers=headers)
     data_contracts = response.json()
     try:
@@ -86,6 +87,7 @@ for offset_loop in list_offsets:
         for ids in data_contracts['records']:
             try:
                 contract_id = ids['fields']['ID']
+                contract_id = str(contract_id)+'.00'
                 confirmation_monitoring = ids['fields']['monitor?'] # Returns true if activated. If not activated it loops into the python Except
                 if confirmation_monitoring is True:
                     list_contracts.append(contract_id)
@@ -97,6 +99,7 @@ for contracts_uploaded in list_contracts:
     print("Uploaded contracts: ", contracts_uploaded)
 
 print(tuple_contracts_to_monitor)
+
 
 # Select entities to upload to GetODK. Note that the label column of the ODK entities table does not accept strange characters. So these are removed in this sql
 # Also duplicate labels should be avoided!
