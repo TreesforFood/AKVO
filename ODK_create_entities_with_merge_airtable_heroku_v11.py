7e315fa0d2888a58b9ca567dacf41b2c6609f6ff
Edmond Muller <info@forestsense.nl>
2025-12-21 21:23:06 +0100

    updated script

--- ODK_create_entities_with_merge_airtable_heroku_v11.py
+++ ODK_create_entities_with_merge_airtable_heroku_v11.py
@@ -407,7 +407,7 @@ for row in rows_dict:
     entities_list.append(entities.copy())
 
 #Connect to ODK central server and use the merge command
-client = Client(config_path="config.toml", cache_path="pyodk_cache.toml")
+client = Client(config_path="/app/tmp/pyodk_config.ini", cache_path="/app/tmp/pyodk_cache.ini")
 
 client.open()
 
