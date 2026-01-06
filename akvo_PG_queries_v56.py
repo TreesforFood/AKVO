0d36ca597e483821d85c925aa265ff0690003f7e
Edmond Muller <info@forestsense.nl>
2026-01-06 16:10:17 +0100

    updated script for editing

--- akvo_PG_queries_v56.py
+++ akvo_PG_queries_v56.py
@@ -1260,7 +1260,7 @@ ALTER TABLE akvo_tree_registration_areas_edits
 ADD COLUMN IF NOT EXISTS fid SERIAL PRIMARY KEY, -- Needed in order to be able to edit in QGIS
 ADD COLUMN IF NOT EXISTS edit_confirmation BOOLEAN, -- QGIS confirmation to process the edits
 ADD COLUMN IF NOT EXISTS chloris_uploaded BOOLEAN, -- Confirmation that the polygons are uploaded to Chloris
-ADD COLUMN IF NOT EXISTS kanop_uploaded BOOLEAN -- Confirmation that the polygons are uploaded to Kanop
+ADD COLUMN IF NOT EXISTS kanop_uploaded BOOLEAN, -- Confirmation that the polygons are uploaded to Kanop
 ADD COLUMN IF NOT EXISTS updated_at TIMESTAMPTZ; -- Add colum if not exits (in case table was already created)'''
 
 conn.commit()
