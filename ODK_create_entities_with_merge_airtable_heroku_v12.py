3ca4c4ca3158dd8861b695a525e6d71bf0a7e3aa
Edmond Muller <info@forestsense.nl>
2026-01-24 20:43:23 +0100

    updated. Sites without geometry are also uploaded to entity list

--- ODK_create_entities_with_merge_airtable_heroku_v12.py
+++ ODK_create_entities_with_merge_airtable_heroku_v12.py
@@ -210,11 +210,10 @@ ELSE 'submitter unknown'
 END AS user_name_enumerator
 
 FROM akvo_tree_registration_areas_updated
---WHERE
---polygon NOTNULL
+WHERE
+polygon NOTNULL
 --AND form_source = 'normal tree registration'
---AND
-WHERE (test = 'This is real, valid data'
+AND (test = 'This is real, valid data'
 OR test = '')
 
 
@@ -294,11 +293,10 @@ ELSE 'submitter unknown'
 END AS user_name_enumerator
 
 FROM akvo_tree_registration_areas_updated
---WHERE
---polygon ISNULL AND centroid_coord NOTNULL
+WHERE
+polygon ISNULL AND centroid_coord NOTNULL
 --AND form_source = 'normal tree registration'
---AND
-WHERE (test = 'This is real, valid data'
+AND (test = 'This is real, valid data'
 OR test = ''))
 
 SELECT
