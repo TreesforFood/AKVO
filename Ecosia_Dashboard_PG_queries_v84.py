8501a3495b6eab6ac439d76d8a7245f7911565ce
Edmond Muller <info@forestsense.nl>
2026-08-20 12:35:00 +0200

    updated

--- Ecosia_Dashboard_PG_queries_v84.py
+++ Ecosia_Dashboard_PG_queries_v84.py
@@ -9364,12 +9364,24 @@ CREATE POLICY ecosia_edit_policy_points ON akvo_tree_registration_areas_edits
 CREATE POLICY ecosia_edit_policy_polygons ON qgis_tree_registration_areas
     FOR ALL
     TO ecosia_editing
-    USING (polygon IS NULL);
+    USING (polygon IS NOT NULL);
 
 CREATE POLICY ecosia_edit_policy_points ON qgis_tree_registration_areas
     FOR ALL
     TO ecosia_editing
     USING (polygon IS NOT NULL);
+---
+
+--- ingevoegd:
+CREATE POLICY ecosia_edit_policy_polygons ON qgis_tree_registration_points
+    FOR ALL
+    TO ecosia_editing
+    USING (polygon IS NULL);
+
+CREATE POLICY ecosia_edit_policy_points ON qgis_tree_registration_points
+    FOR ALL
+    TO ecosia_editing
+    USING (polygon IS NULL);
 ---
 
 CREATE POLICY ecosia_edit_policy ON superset_ecosia_tree_registration_photos
