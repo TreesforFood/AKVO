edf29fa6d3e716806645ab9d6f98f083aa7609b5
Edmond Muller <info@forestsense.nl>
2026-09-18 21:03:32 +0200

    updated

--- ODK_create_registration_entities_with_heroku_v1.py
+++ ODK_create_registration_entities_with_heroku_v1.py
@@ -171,7 +171,7 @@ SELECT
 ROW_NUMBER()OVER(PARTITION BY label ORDER BY label) AS row_number, --Give duplicates a number higher than 1
 label,
 LOWER(organisation) AS name_partner,
-contract_number::varchar(10),
+contract_number::text,
 identifier,
 polygon,
 '' AS geometry
