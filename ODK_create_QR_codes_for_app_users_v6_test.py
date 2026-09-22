6870116a1497570cc6b444107f80523919a72c49
Edmond Muller <info@forestsense.nl>
2026-09-22 12:42:18 +0200

    updated

--- ODK_create_QR_codes_for_app_users_v6_test.py
+++ ODK_create_QR_codes_for_app_users_v6_test.py
@@ -54,15 +54,15 @@ PROJECT_NAME = 'ecosia'
 ADMIN_PASSWORD = "ecosia_change_settings"
 
 
-# Define a writable path for GetODK (/app/tmp is a writable directory on Heroku)
-file_path = "/app/tmp/pyodk_config.ini"
-
-# Create the GetODK directory if it doesn't exist
-os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-# Write the GetODK configuration to the file
-with open(file_path, "w") as file:
-    file.write(file_content)
+# # Define a writable path for GetODK (/app/tmp is a writable directory on Heroku)
+# file_path = "/app/tmp/pyodk_config.ini"
+#
+# # Create the GetODK directory if it doesn't exist
+# os.makedirs(os.path.dirname(file_path), exist_ok=True)
+#
+# # Write the GetODK configuration to the file
+# with open(file_path, "w") as file:
+#     file.write(file_content)
 
 
 # Set the property value to each app user for organisation so that it can be used as a filter for the entity list
