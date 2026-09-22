3bb9236178ee5663d7444b4031ffd7d53b8318b8
Edmond Muller <info@forestsense.nl>
2026-09-22 09:33:00 +0200

    updated

--- ODK_create_QR_codes_for_app_users_v6_test.py
+++ ODK_create_QR_codes_for_app_users_v6_test.py
@@ -50,27 +50,39 @@ default_project_id = {default_project_id}
 
 # Settings for QR code
 PROJECT_ID = 1
-FORMS_TO_ACCESS = ['planting_site_reporting', 'nursery_reporting']
 PROJECT_NAME = 'ecosia'
 ADMIN_PASSWORD = "ecosia_change_settings"
 
 
+# Define a writable path for GetODK (/app/tmp is a writable directory on Heroku)
+file_path = "/app/tmp/pyodk_config.ini"
+
+# Create the GetODK directory if it doesn't exist
+os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+# Write the GetODK configuration to the file
+with open(file_path, "w") as file:
+    file.write(file_content)
+
 
 # Set the property value to each app user for organisation so that it can be used as a filter for the entity list
 # First get the app user id:
-with Client() as client:
-    response_user = client.get(f"/projects/{PROJECT_ID}/app-users")
-    for app_user in response_user.json():
-        app_user_id = app_user['id']
-        print('app_user_id: ', app_user_id)
+client = Client(config_path="/app/tmp/pyodk_config.ini", cache_path="/app/tmp/pyodk_cache.ini")
+client.open()
 
-        # Get the organisation name from the username:
-        organisation = app_user['Display Name']
+response_user = client.get(f"/projects/{PROJECT_ID}/app-users")
 
-        # Set the property value 'organisation' for the user:
-        respons_user_id = client.patch(
-        f"/projects/{PROJECT_ID}/app-users/{app_user_id}",
-        json={
-            "properties": {
-                "organisation": {organisation}}},)
-        #print(response.json())
+for app_user in response_user.json():
+    app_user_id = app_user['id']
+    print('app_user_id: ', app_user_id)
+
+    # Get the organisation name from the username:
+    organisation = app_user['Display Name']
+
+    # Set the property value 'organisation' for the user:
+    respons_user_id = client.patch(
+    f"/projects/{PROJECT_ID}/app-users/{app_user_id}",
+    json={
+        "properties": {
+            "organisation": {organisation}}},)
+    #print(response.json())
