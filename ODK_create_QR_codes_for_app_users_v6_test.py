9d27f56a487a7fedbac4963f8098ea5dcb7df1d7
Edmond Muller <info@forestsense.nl>
2026-09-22 13:47:40 +0200

    updated

--- ODK_create_QR_codes_for_app_users_v6_test.py
+++ ODK_create_QR_codes_for_app_users_v6_test.py
@@ -94,7 +94,7 @@ with Client(config_path="/app/tmp/pyodk_config.ini", cache_path="/app/tmp/pyodk_
         app_user_id = app_user['id']
         print('app_user_id: ', app_user_id)
         organisation = app_user['displayName']
-        organisation_clean = organisation.split('_')
+        organisation_clean = organisation.split('_')[0]
         print(organisation_clean)
         respons_user_id = client.patch(  # Use 'client', not 'Client'
             f"/projects/{PROJECT_ID}/app-users/{app_user_id}",
